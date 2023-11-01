6f746a0d2c18c72a4d30641e8e5b1ed6fd97b158
Anoop508 <32620097+Anoop508@users.noreply.github.com>
2023-11-02 00:42:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 app.get("/",(req, resp)=>{
-    resp.json({message:"Hello Anoop1"})
+    resp.json({message:"Hello Anoop2"})
 })
 
 app.listen(3000);
